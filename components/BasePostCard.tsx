0a85ac057bb8c125caee8665c555022afced259a
Jawad Ali <jawad.ali@repairdesk.co>
2023-01-23 23:06:28 +0500

    make base post card

---
+++ components/BasePostCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function BasePost() {
+  const imgURL =
+    "https://www.freecodecamp.org/news/content/images/size/w2000/2023/01/cover-template--10-.png";
+
+  const profileURL =
+    "https://www.freecodecamp.org/news/content/images/size/w30/2022/06/1654890413623.jpg";
+
+  return (
+    <article className="flex flex-col md:flex-row my-7 max-w-[48rem]">
+      {/* Featured image */}
+      <img
+        src={imgURL}
+        alt="img"
+        className="cursor-pointer w-full md:w-72 md:h-full object-cover h-52 sm:h-80 rounded-sm"
+      />
+      <div className="flex flex-col mt-4 md:mt-0 md:ml-5  flex-1">
+        {/* category */}
+        <a className="text-gray-600 inline-block hover:underline cursor-pointer">
+          #JavaScript
+        </a>
+        {/* title */}
+        <h1 className="font-semibold md:text-lg capitalize hover:underline cursor-pointer w-full max-h-24">
+          How to Submit a Form with JavaScript – JS Submit Button Example
+        </h1>
+        <footer className="my-2 flex items-center text-sm justify-between text-gray-600 mr-4">
+          <div className="flex items-center gap-x-3">
+            {/* user profile pic */}
+            <img
+              src={profileURL}
+              alt="profile photo"
+              className="w-8 h-8  cursor-pointer object-cover"
+            />
+            {/* user name */}
+            <p className="text-gray-700 text-sm">User Name</p>
+          </div>
+          {/* time stamp */}
+          <p className="text-gray-500 text-xs">3 Days ago</p>
+        </footer>
+      </div>
+    </article>
+  );
+}
+
+export default BasePost;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import BasePost from "../components/BasePostCard"
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +9,13 @@ const Home: NextPage = () => {
       <Head>
         <title>Nextjs Blog</title>
       </Head>
-      <div>al</div>
+      {/* posts */}
+      <section className="w-full md:flex md:flex-col md:items-center px-5">
+        <BasePost></BasePost>
+        <BasePost></BasePost>
+        <BasePost></BasePost>
+        <BasePost></BasePost>
+      </section>
     </div>
   );
 };
